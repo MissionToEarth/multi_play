29645bbd1b17e84f7268e4274def2ada69671275
t-harada <harada@platinum-egg.com>
2019-03-26 23:11:06 +0900

    edit photon logic

--- test_photon_console/src/HANetwork.hpp
+++ test_photon_console/src/HANetwork.hpp
@@ -20,19 +20,29 @@
 #define HA
 
 #include "ha_boot.hpp"
-#include <vector>
-#include <string>
+#include "HAPhoton/HAPhotonConstants.hpp"
 
-//#define USE_EG_PHOTON
-//
-//#ifdef USE_EG_PHOTON
 #include "Common-cpp/inc/Common.h"
 #include "LoadBalancing-cpp/inc/Client.h"
 #include "LoadBalancing-cpp/inc/Listener.h"
-//#endif
+
+#include <vector>
+#include <string>
 
 
 //NS_HANETWORK_START
+
+using EGCOMMON = ExitGames::Common::JString;
+
+class HALogListener : public ExitGames::Common::BaseListener
+{
+public:
+    HALogListener() {};
+    virtual void debugReturn(int debugLevel, const ExitGames::Common::JString& string) override
+    {
+        std::cout << string.toString().trim().UTF8Representation().cstr() << std::endl;
+    }
+};
 
 
 #ifdef USE_EG_PHOTON
@@ -58,31 +68,63 @@ std::vector<std::string> requestAvailableRegions()
     return regions;
 };
 
-
+#define LOG_I(x)         EGLOG(ExitGames::Common::DebugLevel::INFO, x);
 class HAListener : public ExitGames::LoadBalancing::Listener
 {
+    ExitGames::Common::Logger mLogger; // accessed by EGLOG()
+
 public:
+    HAListener()
+    {
+        //TODO:
+        static HALogListener h;
+        mLogger.setListener(h);
+        mLogger.setDebugOutputLevel(4);
+        LOG_I(L"コンストラクタン！");
+    }
     virtual ~HAListener(void){};
 
     // receive and print out debug out here
-    virtual void debugReturn(int debugLevel, const ExitGames::Common::JString& string) {};
+    virtual void debugReturn(int debugLevel, const ExitGames::Common::JString& string) {
+        LOG_I(L"debug return. "+ string.toString());
+    };
     
     // implement your error-handling here
-    virtual void connectionErrorReturn(int errorCode) {};
-    virtual void clientErrorReturn(int errorCode) {};
-    virtual void warningReturn(int warningCode) {};
-    virtual void serverErrorReturn(int errorCode) {};
+    virtual void connectionErrorReturn(int errorCode) {
+        LOG_I(L"connection error code.");
+        std::cout << errorCode << std::endl;
+    };
+    virtual void clientErrorReturn(int errorCode) {
+        LOG_I(L"client error code.%s");
+        std::cout << errorCode << std::endl;
+    };
+    virtual void warningReturn(int warningCode) {
+        LOG_I(L"warning return");
+        std::cout << warningCode << std::endl;
+    };
+    virtual void serverErrorReturn(int errorCode) {
+        LOG_I(L"server error return");
+        std::cout << errorCode << std::endl;
+    };
     
     // events, triggered by certain operations of all players in the same room
     virtual void joinRoomEventAction(int playerNr, const ExitGames::Common::JVector<int>& playernrs, const ExitGames::LoadBalancing::Player& player) {};
     virtual void leaveRoomEventAction(int playerNr, bool isInactive) {};
     
-    virtual void customEventAction(int playerNr, nByte eventCode, const ExitGames::Common::Object& eventContent) {};
+    virtual void customEventAction(int playerNr, nByte eventCode, const ExitGames::Common::Object& eventContent) {
+        EGLOG(ExitGames::Common::DebugLevel::INFO, L"イベントを受信した.");
+    };
     
     // callbacks for operations on server
-    virtual void connectReturn(int errorCode, const ExitGames::Common::JString& errorString, const ExitGames::Common::JString& region, const ExitGames::Common::JString& cluster) {};
-    virtual void disconnectReturn(void) {};
-    virtual void createRoomReturn(int localPlayerNr, const ExitGames::Common::Hashtable& roomProperties, const ExitGames::Common::Hashtable& playerProperties, int errorCode, const ExitGames::Common::JString& errorString) {};
+    virtual void connectReturn(int errorCode, const ExitGames::Common::JString& errorString, const ExitGames::Common::JString& region, const ExitGames::Common::JString& cluster) {
+        EGLOG(ExitGames::Common::DebugLevel::INFO, L"connect return.");
+    };
+    virtual void disconnectReturn(void) {
+        EGLOG(ExitGames::Common::DebugLevel::INFO, L"disconnect return.");
+    };
+    virtual void createRoomReturn(int localPlayerNr, const ExitGames::Common::Hashtable& roomProperties, const ExitGames::Common::Hashtable& playerProperties, int errorCode, const ExitGames::Common::JString& errorString) {
+        EGLOG(ExitGames::Common::DebugLevel::INFO, L"create room return.");
+    };
     virtual void joinOrCreateRoomReturn(int localPlayerNr, const ExitGames::Common::Hashtable& roomProperties, const ExitGames::Common::Hashtable& playerProperties, int errorCode, const ExitGames::Common::JString& errorString) {};
     virtual void joinRoomReturn(int localPlayerNr, const ExitGames::Common::Hashtable& roomProperties, const ExitGames::Common::Hashtable& playerProperties, int errorCode, const ExitGames::Common::JString& errorString) {};
     virtual void joinRandomRoomReturn(int localPlayerNr, const ExitGames::Common::Hashtable& roomProperties, const ExitGames::Common::Hashtable& playerProperties, int errorCode, const ExitGames::Common::JString& errorString) {};
@@ -90,9 +132,6 @@ public:
     virtual void joinLobbyReturn(void) {};
     virtual void leaveLobbyReturn(void) {};
 };
-
-HAListener mls;
-ExitGames::LoadBalancing::Client mLoadBalancingClient(mls, "", "");
 
 //TODO:
 class HANetworkLogic
@@ -104,34 +143,76 @@ public:
     void connect(void);
     void disconnect(void);
     void run(void);
+    void createRoom(const ExitGames::Common::JString& roomName, nByte maxPlayers);
+    void join();
+    void send();
 private:
-//    ExitGames::LoadBalancing::Client mLoadBalancingClient;
-//    HAListener mListener; // your implementation of the ExitGames::LoadBalancing::Listener interface
-//    ExitGames::Common::Logger mLogger; // accessed by EGLOG()
+    ExitGames::LoadBalancing::Client mLoadBalancingClient;
+    HAListener mListener; // your implementation of the ExitGames::LoadBalancing::Listener interface
+    ExitGames::Common::Logger mLogger; // accessed by EGLOG()
 };
 
 HANetworkLogic::HANetworkLogic(const ExitGames::Common::JString& appID, const ExitGames::Common::JString& appVersion)
-//    : mLoadBalancingClient(mListener, appID, appVersion)
+    : mLoadBalancingClient(mListener, appID, appVersion)
 {
+    //TODO: staticとかやべーだろ。
+    auto p = std::unique_ptr<HALogListener>();
+    static HALogListener h;
+    mLogger.setListener(h);
+    mLogger.setDebugOutputLevel(4);
 }
 
 void HANetworkLogic::connect(void)
 {
+    EGLOG(ExitGames::Common::DebugLevel::INFO, L"EGLOG.");
+
     // connect() is asynchronous - the actual result arrives in the Listener::connectReturn() or the Listener::connectionErrorReturn() callback
-//    if(!mLoadBalancingClient.connect())
-//        EGLOG(ExitGames::Common::DebugLevel::ERRORS, L"Could not connect.");
+    if(!mLoadBalancingClient.connect())
+        EGLOG(ExitGames::Common::DebugLevel::ERRORS, L"Could not connect.");
 }
 
 void HANetworkLogic::disconnect(void)
 {
-
+    mLoadBalancingClient.disconnect();
+    EGLOG(ExitGames::Common::DebugLevel::INFO, L"disconnect.");
 }
 
 void HANetworkLogic::run(void)
 {
-
+    mLoadBalancingClient.service();
+//    EGLOG(ExitGames::Common::DebugLevel::INFO, L"net logic run.");
 }
 
+void HANetworkLogic::createRoom(const ExitGames::Common::JString& roomName, nByte maxPlayers)
+{
+    mLoadBalancingClient.opCreateRoom(roomName, ExitGames::LoadBalancing::RoomOptions().setMaxPlayers(maxPlayers));
+}
+
+void HANetworkLogic::join()
+{
+    //TODO: search room.
+    
+    
+    //is random.
+//    // join random rooms easily, filtering for specific room properties, if needed
+//    ExitGames::Common::Hashtable expectedCustomRoomProperties;
+//
+//    // custom props can have any name but the key must be string
+//    expectedCustomRoomProperties.put(L"room01", 1);
+//
+//    // joining a random room with the map we selected before
+//    mLoadBalancingClient.opJoinRandomRoom(expectedCustomRoomProperties);
+}
+
+void HANetworkLogic::send()
+{
+    //イベントコードは200以下にすること。
+    nByte eventCode = 1; // use distinct event codes to distinguish between different types of events (for example 'move', 'shoot', etc.)
+    assert(eventCode<=200);
+    ExitGames::Common::Hashtable evData; // organize your payload data in any way you like as long as it is supported by Photons serialization
+    bool sendReliable = false; // send something reliable if it has to arrive everywhere
+    mLoadBalancingClient.opRaiseEvent(sendReliable, evData, eventCode);
+}
 // class HANetworkLogic
 // {
 
--- test_photon_console/src/main.cpp
+++ test_photon_console/src/main.cpp
@@ -1,5 +1,6 @@
 #include "HANetwork.hpp"
 
+
 #include <iostream>
 #include <string>
 #include <regex>
@@ -24,7 +25,8 @@ namespace d3
         inline void end()
         {
             auto end = std::chrono::system_clock::now();
-            double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - _start).count();            
+            double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - _start).count();
+            std::cout << elapsed << std::endl;
         }
 	};
 }
@@ -39,40 +41,66 @@ static long getCurrentMillSecond() {
     return lLastTime;
 }
 
-void create_room()
-{
-
-}
-
-
 int main(int argc, char** argv) 
 {
 	using std::string;
 	using std::cin;
     using std::cout;
+    using std::endl;
 
-    cout << "Input Photon-App-Key";
-	string str;
-	cin >> str;
-    cout << str;
+    cout << "Input Photon-App-Key" << endl;
+//    string str;
+//    cin >> str;
+//    cout << str;
 
     long _animationInterval = (1.0f/60.0f*1000.0f);
 
     long lastTime = 0L;
     long curTime = 0L;
 
+    HANetworkLogic photon(appID, appVersion);
+    
 	std::string key_input;
 	while(1)
 	{
         lastTime = getCurrentMillSecond();
 
         //　テストロジック
+        if (key_input == "run")
+        {
+            photon.run();
+            photon.send();
+        }
+        else
         {
             cout << "." ;
             cin >> key_input;
-            if( key_input == "exit" )               { break; }
-            else if( key_input == "create_room" )   { create_room(); }
-            else if( key_input == "connect" )       {  }
+            cin.clear();
+            cin.ignore();
+            if( key_input == "exit" )
+            {
+                photon.disconnect();
+                break;
+            }
+            else if( key_input == "create_room" )
+            {
+                photon.createRoom(L"room01", 9);
+            }
+            else if( key_input == "connect" )
+            {
+                d3::Clock cc;
+                cc.start();
+                photon.connect();
+                cc.end();
+            }
+            else if (key_input=="send")
+            {
+                photon.send();
+            }
+            else
+            {
+//                key_input = "";
+            }
         }
 
         curTime = getCurrentMillSecond();
